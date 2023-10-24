baaf90e1f1a3d1933c7f13e647bd3bc46f52319f
tangjiahui <tjhaaa@163.com>
2023-10-24 14:00:44 +0800

    fix(script/utils):使用flat扁平化数组

--- script/utils/index.js
+++ script/utils/index.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 // 同步运行webpack打包任务
 function runWebpackSync(...args) {
   let i = 0;
-  const configList = args.reduce((res, cur) => res.concat(cur), []);
+  const configList = args.flat(Infinity);
 
   function run() {
     const config = configList[i++];
